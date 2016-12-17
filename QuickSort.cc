0b3f8f636d5c4009aab41f8ec8e943e2a763b152
Frederic Heem <f@f.com>
2016-12-17 17:44:42 +0000

    quick sort cleanup

--- QuickSort.cc
+++ QuickSort.cc
@@ -10,25 +10,18 @@ using namespace std;
 
 template<typename Iterator>
 Iterator Partition(Iterator begin, Iterator end){
-    
     auto size = std::distance(begin, end);
     if(size <= 1){
         return begin;
     }
-    cout << "Partition size " << size << ", begin: " << *begin << ", end: " << *(end - 1) << endl;
     
-    print(begin, end);
-
     Iterator left = begin;
     Iterator right = end - 1;
     //Pivot in the middle, could be anyone really, this affects performance by the way.
     auto pivotIndex = size / 2;
     auto pivotValue = *std::next(begin, pivotIndex);
     
-    cout << "Partition pivot index: " << pivotIndex << ", pivotValue: " << pivotValue << endl;
-    
     while(left <= right){
-        //cout << "Partition before left: " << *left << ", right: " << *right << endl;
         while(*left < pivotValue){
             left++;
         }
@@ -37,10 +30,7 @@ Iterator Partition(Iterator begin, Iterator end){
             right--;
         }
         
-        //cout << "Partition after left: " << *left << ", right: " << *right << endl;
         if(left < right){
-            cout << "Partition  SWAP: " << *left << " with " << *right << endl;
-            //print(begin, end);
             std::swap(*left, *right);    
             left++; 
             right--;    
@@ -48,44 +38,28 @@ Iterator Partition(Iterator begin, Iterator end){
             break;
         }
     }
-   
     
-    cout << "Partition DONE" << endl;
-    print(begin, end);
     return left;
 }
 
 template<typename Iterator>
-void QuickSort(Iterator begin, Iterator end, int &level){
-    level++;
-    cout << "QuickSort begin " << *begin << ", size: " << std::distance(begin, end) << ", level: " << level << endl;
+void QuickSort(Iterator begin, Iterator end){
     if(std::distance(begin, end) <= 1){
         return;
     }
 
-    print(begin, end);
-    
-    if(level > 10){
-        cerr << "max level reached " << level << endl;
-        return;
-    }
-    
     Iterator splitIt = Partition(begin, end);
-    
-    QuickSort(begin, splitIt, level);
-    QuickSort(splitIt, end, level);
+    QuickSort(begin, splitIt);
+    QuickSort(splitIt, end);
 }
 
 template<class T>
 void QuickSort(T& arr){
-    cout << "Main QuickSort size: " << arr.size() << endl;
     if(arr.size() <= 1){
         return;
     }
     
-    int level = 0;
-    
-    QuickSort(arr.begin(), arr.end(), level);
+    QuickSort(arr.begin(), arr.end());
 }
 
 bool testQuickSort(vector<int> &array){
@@ -97,8 +71,6 @@ bool testQuickSort(vector<int> &array){
     assert(equal);
     return equal;
 }
-
-
 
 int main() {
     cout << "Hello QuickSort" << endl;
