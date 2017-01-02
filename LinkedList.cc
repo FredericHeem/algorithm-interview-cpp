4ac214574f17738bec496811a286f88feac6c6e0
Frederic Heem <f@f.com>
2017-01-02 16:42:51 +0000

    more on LinkedList

--- LinkedList.cc
+++ LinkedList.cc
@@ -8,7 +8,6 @@ struct Node {
     T value;
     Node* next;
     Node(T v) : value(v), next(nullptr){
-        
     }
 };
 
@@ -20,7 +19,7 @@ struct List {
         
     }
     void push_back(const T& value){
-        Node<T>* node = new Node<T>(value);
+        auto node = new Node<T>(value);
         if(head == nullptr){
             head = node;
             tail = node;
@@ -31,12 +30,55 @@ struct List {
     }
     int size(){
         int size = 0;
-        Node<T>* current = head;
+        auto current = head;
         while(current != nullptr){
             current = current->next;
             size++;
         }
         return size;
+    }
+    void clear(){
+        auto current = head;
+        while(current != nullptr){
+            auto next = current->next;
+            delete current;
+            current = next;
+        }
+        head = nullptr;
+        tail = nullptr;
+    }
+    
+    void reverse(){
+        tail = head;
+        auto current = head;
+        Node<T>* previous = nullptr;
+        while(current != nullptr){
+            auto next = current->next;
+            current->next = previous;
+            previous = current;
+            current = next;
+        }
+        head = previous;
+    }
+    void deleteNodeByValue(const T& val){
+        auto current = head;
+        Node<T>* previous = nullptr;
+        while(current != nullptr){
+            auto next = current->next;
+            if(current->value == val){
+                if(previous != nullptr){
+                    previous->next = next;
+                } else {
+                    head = next;
+                }
+                if(current == tail){
+                    tail = previous;
+                }
+                delete current;
+            }
+            previous = current;
+            current = next;
+        }
     }
 };
 
@@ -46,7 +88,25 @@ int main() {
     assert(list.head == nullptr);
     assert(list.size() == 0);
     list.push_back(1);
+    assert(list.head->value == 1);
+    assert(list.tail->value == 1);
+    assert(list.size() == 1);
+    list.push_back(2);
+    assert(list.size() == 2);
+    
+    list.reverse();
+    assert(list.size() == 2);
+    assert(list.head->value == 2);
+    
+    list.deleteNodeByValue(1);
     assert(list.size() == 1);
     
+    list.deleteNodeByValue(2);
+    assert(list.size() == 0);
+    
+    list.clear();
+    assert(list.size() == 0);
+    assert(list.head == nullptr);
+    assert(list.tail == nullptr);
     
 }
