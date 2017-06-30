7e973f909c7571af746a48986378e6c35439b4bf
Frederic Heem <frederic.heem@gmail.com>
2017-06-30 21:47:39 +0100

    add factorial

--- combinatorics/Permutation.cc
+++ combinatorics/Permutation.cc
@@ -12,14 +12,21 @@ void display(const vector<string> &list){
     cout << endl;
 }
 
+int factorial(int i){
+    if(i > 1){
+        return i * factorial(i - 1);
+    } 
+    return 1;
+}
+
 int main()
 {
     std::cout << "Permutation\n";
 
-    vector<string> ruleA {"A1", "A2", "A3", "A4"};
-    std::string s = "aba";
-    std::sort(s.begin(), s.end());
+    vector<string> ruleA {"A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A10", "A11", "A12"};
+    int permutationSize = factorial(ruleA.size());
+    cout << "permutationSize " << permutationSize << endl;
     do {
-        display(ruleA);
+        //display(ruleA);
     } while(std::next_permutation(ruleA.begin(), ruleA.end()));
 }
